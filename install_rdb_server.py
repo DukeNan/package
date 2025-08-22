be86a3c3627f5a6a6abcc1fa6cb39ca94a9fbcbb
shaun <dukenan006@163.com>
2025-08-22 11:17:08 +0800

    rdb server初始化支持修改配置文件IP和初始化initiate

--- install_rdb_server.py
+++ install_rdb_server.py
@@ -1,4 +1,6 @@
+import ipaddress
 import tarfile
+from pathlib import Path
 
 from constants import PROJECT_DIR, PackageFilenameEnum
 from utils.check import HostEnvironmentDetection
@@ -68,6 +70,50 @@ class Installer:
             logger.error(f"Failed to start aio-speedd: {result.stderr}")
         logger.info("aio-speedd is started")
 
+    def _is_valid_ipv4(self, ip: str) -> bool:
+        try:
+            ipaddress.IPv4Address(ip)
+            return True
+        except ipaddress.AddressValueError:
+            return False
+
+    def _replace_aio_env(self) -> None:
+        """
+        替换 aio.env 文件中的 127.0.0.1 为实际的 server ip
+        """
+        # 读取 aio.env 文件
+        aio_env_file = Path("/opt/aio/cfg/aio.env")
+        if not aio_env_file.exists():
+            logger.error(f"aio.env file not found: {aio_env_file.as_posix()}")
+            return
+        # 读取 aio.env 文件内容
+        content = aio_env_file.read_text(encoding="utf-8")
+        if "127.0.0.1" not in content:
+            logger.info(f"{aio_env_file.as_posix()} is not modified")
+            return
+        # 替换 aio.env 文件内容
+        while True:
+            input_str = input(
+                f"Please input the server ipv4 address: (example: 192.168.1.100)"
+            )
+            input_str = input_str.strip()
+            if "127.0.0.1" in input_str:
+                logger.error(f"Invalid ip: {input_str}")
+                continue
+            if not self._is_valid_ipv4(input_str):
+                logger.error(f"Invalid ip: {input_str}")
+                continue
+            content = content.replace("127.0.0.1", input_str)
+            break
+        aio_env_file.write_text(content, encoding="utf-8")
+        logger.info(f"{aio_env_file.as_posix()} is modified")
+
+    def init_service(self) -> None:
+        """
+        初始化服务
+        """
+        Command(["systemctl", "start", "aio.service"]).run(original=True, display=True)
+
     def run(self) -> None:
         if not self.host_environment_detection.check():
             return
@@ -79,6 +125,8 @@ class Installer:
             return
         self._install_rpm()
         self._start_aio_speedd()
+        self._replace_aio_env()
+        self.init_service()
 
 
 if __name__ == "__main__":
